32b730a75d7925cc460f8a8c262faa157a7edd95
My Edu Digital <krishnamodi@myedudigital.com>
2026-08-02 15:13:36 +0530

    Updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,6 @@ import {
 import { ThemeProvider } from "@/contexts/ThemeContext";
 
 import HomePage from "@/pages/homePage";
-import AboutPage from "@/pages/aboutPage";
-import PumpsPage from "@/pages/pumpsPage";
-import CablesPage from "@/pages/cablesPage";
-import DrivesPage from "@/pages/drivesPage";
-import MotorsPage from "@/pages/motorsPage";
-import SparesPage from "@/pages/sparesPage";
-import SignInPage from "@/pages/signInPage";
-import GearBoxesPage from "@/pages/gearBoxesPage";
 import LenisScrollProvider from "@/components/LenisScrollProvider";
 
 function App() {
@@ -24,14 +16,6 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/* <Route path="/about" element={<AboutPage />} />
-            <Route path="/motors" element={<MotorsPage />} />
-            <Route path="/drives" element={<DrivesPage />} />
-            <Route path="/pumps" element={<PumpsPage />} />
-            <Route path="/gear-boxes" element={<GearBoxesPage />} />
-            <Route path="/cables" element={<CablesPage />} />
-            <Route path="/spares" element={<SparesPage />} />
-            <Route path="/sign-in" element={<SignInPage />} /> */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Router>
