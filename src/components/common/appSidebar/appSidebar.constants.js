d494496ee45cd9edeeb4c516b14cef2690c18763
me-krishnamodi <dhruvghadiali@hotmail.com>
2026-09-21 03:10:29 +0530

    update sidebar option

--- src/components/common/appSidebar/appSidebar.constants.js
+++ src/components/common/appSidebar/appSidebar.constants.js
@@ -32,10 +32,10 @@ export const SIDEBAR_NAV_ITEMS_BY_ROLE = {
     { title: "Purchase orders", url: ROUTES.PURCHASES, icon: ShoppingCart },
     { title: "Sales", url: ROUTES.SALES, icon: Receipt },
     { title: "EUOB", url: ROUTES.PURCHASE_CREDITS, icon: HandCoins },
-    { title: "Tally Debiters", url: ROUTES.TALLY_DEBITERS, icon: BookOpen },
     { title: "Products", url: ROUTES.PRODUCTS, icon: Boxes },
     { title: "Companies", url: ROUTES.COMPANIES, icon: Building2 },
     { title: "Settings", url: ROUTES.SETTINGS, icon: Settings },
+    { title: "Tally", url: ROUTES.TALLY_DEBITERS, icon: BookOpen },
   ],
   [ROLE_PATHS.WAREHOUSE_MANAGER]: [
     { title: "Dashboard", url: ROUTES.DASHBOARD, icon: LucideHome },
